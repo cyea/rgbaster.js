fd6140e54cf1102360d49ffc5164670efcbe9a09
jingchaofang <jing@turingca.com>
2017-12-21 19:00:50 +0800

    update the repository

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rgbaest.js
+
 获取图片指定区域色值：rgbaest.js 场景实践
---
+++ build/index.js
@@ -0,0 +1,24 @@
+var UglifyJS = require("uglify-js");
+var fs = require('fs');
+var path = require('path');
+
+const RELEASE_ROOT_PATH = 'dist';
+const SOURCE_ROOT_PATH = 'src';
+
+function resolvePath(p) {
+  return path.resolve(__dirname, '../', p);
+}
+
+if (!fs.existsSync(resolvePath(RELEASE_ROOT_PATH))) {
+    fs.mkdirSync(resolvePath(RELEASE_ROOT_PATH));
+}
+
+var options = {
+  mangle: {
+    properties: true,
+  }
+}
+
+fs.writeFileSync(resolvePath("dist/rgbaster.min.js"), UglifyJS.minify({
+  "rgbaster.js": fs.readFileSync(resolvePath("src/rgbaster.js"), "utf8")
+}, options).code, "utf8");
